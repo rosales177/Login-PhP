425a76d94ad44cbe9f423182c7df7aa7d4884a1a
Geraldo Oneil Rosales Poma <rosalespoma15@gmail.com>
2021-07-16 17:27:10 -0500

    Se hizo un parche de Ajax

--- js/app.js
+++ js/app.js
@@ -8,13 +8,13 @@ $(function(){
       user: $('#user').val(),
       pass: $('#pass').val()
     };
-
+    
     console.log(DatosPost);
     /*Modificar el Url antes de ejecutar*/
     //[Url Remota]
-    //let url = "/Login/php/controlador.php";
+    let url = "/Login/php/controlador.php";
     //[Url Local]
-    let url = "/login-php/php/controlador.php";
+    //let url = "/login-php/php/controlador.php";
 
     $.post(url, DatosPost, function(response){
         console.log(response);
